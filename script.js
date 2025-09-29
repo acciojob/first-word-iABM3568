1219dcb86af2a0c8fe009d28a83f41a8ca6d2b53
Abhishek Mishra <iabm3568@gmail.com>
2025-09-29 11:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function firstWord(s) {
   // your code here
+
+	// Case 1: empty string
+  if (s.length === 0) {
+    return "";
+  }
+
+  let word = "";
+  let started = false; // track if first non-space started
+
+  for (let i = 0; i < s.length; i++) {
+    let ch = s[i];
+
+    // Ignore leading spaces 
+    if (!started && ch === " ") {
+      continue;
+    }
+
+    // If we hit a space *after* starting, stop
+    if (started && ch === " ") {
+      break;
+    }
+
+    // Add character to word
+    word += ch;
+    started = true;
+  }
+
+  return word;
+	
 }
 
 // Do not change the code below
